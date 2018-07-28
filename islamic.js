0d790978b1554ebd1f5162b64975cfb05f775cb5
omaralshmari <41575972+omaralshmari@users.noreply.github.com>
2018-07-28 13:35:00 +0300

    Update islamic.js

--- islamic.js
+++ islamic.js
@@ -1,73 +1,35 @@
-"use strict";
-const Discord = require( 'discord.js' );
-const ytdl = require('ytdl-core');
-const client = new Discord.Client( );
-const settings = require("./settings.json");
-client.login(settings.token);
-
-var connections = { }
-var _she5 = {
-"ماهر المعيقلي": "https://www.youtube.com/watch?v=Ktync4j_nmA",
-"مشاري العفاسي": "https://www.youtube.com/watch?v=2etOS_nwTr0",
-"عبد الباسط عبد الصمد": "https://www.youtube.com/watch?v=vqXLGtZcUm8",
-"ياسر الدوسري": "https://www.youtube.com/watch?v=WYT0pQne-7w"
+{
+  "//1": "describes your app and its dependencies",
+  "//2": "https://docs.npmjs.com/files/package.json",
+  "//3": "updating this file will download and update your packages",
+  "name": "hello-express",
+  "version": "0.0.1",
+  "description": "A simple Node app built on Express, instantly up and running.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "canvas": "^1.6.11",
+    "discord.js": "^11.3.2",
+    "export": "^0.1.337",
+    "express": "^4.16.3",
+    "fs": "^0.0.2",
+    "json": "^9.0.6",
+    "object": "^0.2.0",
+    "require": "^2.4.20"
+  },
+  "engines": {
+    "node": "8.x"
+  },
+  "repository": {
+    "url": "https://glitch.com/edit/#!/hello-express"
+  },
+  "license": "MIT",
+  "keywords": [
+    "node",
+    "glitch",
+    "express"
+  ]
 }
-
-var _sheo5 = [
-"ماهر المعيقلي",
-"مشاري العفاسي",
-"عبد الباسط عبد الصمد",
-"ياسر الدوسري"
-]
-
-
-var voice = "quran"
-
-
-function repeatQuran(quranLink,connection){
-    const dispatcher = connection.playStream(ytdl(quranLink, {filter: 'audioonly'}), {volume:1,seek:0, passes : 1});
-    dispatcher.on('end', ( ) => {
-		if( connection == undefined ) return;
-		repeatQuran(quranLink,connection)
-    })
-}
-
-client.on('message', ( message ) => {
-	let command = message.content.split(' ')[0];
-	let args = message.content.split(' ').slice(1);
-	if( command == "قران" || command == "قرآن" ){
-		var sheo5 = "";
-		for( var i = 0; i < _sheo5.length; i++ ){
-			sheo5 += "\n**" + (i+1) + "- " + _sheo5[i] + " **";
-		}
-		message.reply( '**يرجى كتابة اسم الشيخ المراد سماع تلاوته**\n **:الشيوخ المتوفرون هم**'+sheo5)
-		message.channel.awaitMessages(msg => msg.author == message.author,{ max: 1 })
-		.then( messages => {
-			var message = messages.first();
-			if( _she5[message.content] ){
-				if( connections[ message.guild ] ){
-					message.reply("**:white_check_mark: تم بدء التلاوة مع الشيخ "+ message.content +" **");
-					repeatQuran(_she5[message.content],connections[ message.guild ]);
-				} else{ 
-				
-					message.reply("**:timer: جاري دخول القناة ...**").then( m => {
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { 
-							m.edit("**:white_check_mark: تم بدء التلاوة مع الشيخ "+ message.content +" **");
-							connections[ message.guild ] = connection;
-							repeatQuran(_she5[message.content],connections[ message.guild ]);
-		})
-						};
-					});
-				}
-			} else {
-				message.reply("**:x: لم يتم ايجاد الشيخ**")
-			}
-			
-		})
-		.catch( err => { err; } );
-	
-	}
-});
-
+client.login(process.env.BOT_TOKEN); 
